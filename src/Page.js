6d488e33c9812aa217f7b809a4bfa3e9f6b611f3
Cohee <18619528+Cohee1207@users.noreply.github.com>
2024-07-10 09:25:51 +0300

    Update react-quill library

--- src/Page.js
+++ src/Page.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 import React from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 /**
  * Component for displaying a page in the notebook.
